928a7a0a45e69592a01538d53d383bbc0c76f355
rajkamal3 <rkchenumalla@gmail.com>
2021-08-14 18:25:56 +0530

    backend added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,20 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+    useEffect(() => {
+        axios
+            .post(`https://ewmw.edelweiss.in/api/Market/optionchainguest`, {
+                aTyp: 'OPTIDX',
+                exp: '18 Aug 2021',
+                uSym: 'BANKNIFTY'
+            })
+            .then(res => {
+                console.log(res);
+            });
+    }, []);
+
     return <div>2Pac</div>;
 }
 
