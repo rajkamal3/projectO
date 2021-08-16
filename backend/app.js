6154fbe81f9ca923a9ef02a553a1cdd914b8fd30
rajkamal3 <rkchenumalla@gmail.com>
2021-08-16 22:17:22 +0530

    testing

--- backend/app.js
+++ backend/app.js
@@ -104,7 +104,7 @@ app.get('/api/marketOpens', getData, marketOpens);
 app.get('/api/marketCloses', getData, marketCloses);
 
 cron.schedule(
-    '09 22 * * 1-5',
+    '19 22 * * 1-5',
     async () => {
         axios.get('https://stokr-projecto.herokuapp.com/api/marketOpens');
     },
@@ -115,7 +115,7 @@ cron.schedule(
 );
 
 cron.schedule(
-    '10 22 * * 1-5',
+    '20 22 * * 1-5',
     async () => {
         axios.get('https://stokr-projecto.herokuapp.com/api/marketCloses');
     },
